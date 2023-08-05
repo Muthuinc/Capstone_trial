eaec834e5bfe95ba7dbb4a129c969b7dcc422525
Muthu ramalingam <128946991+Muthuinc@users.noreply.github.com>
2023-08-05 11:35:59 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}"
-export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
+#export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}"
+#export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
 
 a=$(aws ec2 describe-instances --region ap-south-1 --filters "Name=tag:Env,Values=prod" --query 'Reservations[].Instances[].PublicIpAddress' --output text)
 export psw1="$1"
-ssh -o StrictHostKeyChecking=no -i "$psw1" ubuntu@$a <<EOF
+ssh -o StrictHostKeyChecking=no -i "$SSH_KEY" "$ubuntu"@$a <<EOF
 
 
 sudo docker-compose up -d
