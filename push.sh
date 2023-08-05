a2141325fdf698e092ba48797174b7b4f875bd55
Muthu ramalingam <128946991+Muthuinc@users.noreply.github.com>
2023-08-05 19:54:22 +0530

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 echo "$DOCKER_CRED_PSW" | docker login -u $DOCKER_CRED_USR --password-stdin
-GIT_COMMIT=$(git rev-parse --short HEAD)
+
 docker push muthuinc/react2:"${GIT_COMMIT}"
 
 docker logout
