03071976a67e26bda19cf260c75b22218a29409e
Muthu ramalingam <128946991+Muthuinc@users.noreply.github.com>
2023-08-05 19:22:15 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
 # building the docker image
+GIT_COMMIT=$(git rev-parse --short HEAD)
 
-docker build -t muthuinc/react2:v1 .
+docker build -t muthuinc/react2:"${GIT_COMMIT}" .
 
 echo "success"
 
