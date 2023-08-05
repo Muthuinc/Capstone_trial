693e56dc284398d2933fd89fdd520237c7354bab
Muthu ramalingam <128946991+Muthuinc@users.noreply.github.com>
2023-08-05 16:09:03 +0530

    Update create.sh

--- create.sh
+++ create.sh
@@ -7,6 +7,8 @@ terraform init && terraform apply --auto-approve
 
 echo " success"
 
+sleep 10
+
 a=$(aws ec2 describe-instances --region ap-south-1 --filters "Name=tag:Env,Values=prod" --query 'Reservations[].Instances[].PublicIpAddress' --output text)
 
 echo "$a"
