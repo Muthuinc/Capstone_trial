50bd2c903f33b5149369ee0e69d5dac2425fd29c
Muthu ramalingam <128946991+Muthuinc@users.noreply.github.com>
2023-08-05 10:51:03 +0530

    Update create.sh

--- create.sh
+++ create.sh
@@ -9,6 +9,8 @@ echo " success"
 
 a=$(aws ec2 describe-instances --region ap-south-1 --filters "Name=tag:Env,Values=prod" --query 'Reservations[].Instances[].PublicIpAddress' --output text)
 
+echo "$a"
+
 export psw1="$1"
 
 scp -o StrictHostKeyChecking=no -i "$SSH_KEY" docker-compose.yml ubuntu@$a:/home/ubuntu
