b51ed13facb31a549036f5bf151f8441042e6be0
Muthu ramalingam <128946991+Muthuinc@users.noreply.github.com>
2023-08-05 11:01:53 +0530

    Update create.sh

--- create.sh
+++ create.sh
@@ -13,7 +13,7 @@ echo "$a"
 
 export psw1="$1"
 
-scp -o StrictHostKeyChecking=no -i "$SSH_KEY" docker-compose.yml ubuntu@$a:/home/ubuntu
+scp -o StrictHostKeyChecking=no -i "$SSH_KEY" docker-compose.yml "$ubuntu"@$a:/home/ubuntu
 
 #ssh -o StrictHostKeyChecking=no -i "$psw1" ubuntu@$a <<EOF
 
