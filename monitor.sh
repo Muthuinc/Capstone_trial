adb1eda7dfbf1182290a277954da5f94db1b63ac
Muthu ramalingam <128946991+Muthuinc@users.noreply.github.com>
2023-08-07 11:20:24 +0530

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}"
-export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
+#export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}"
+#export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
 
-export psw1="$1"
+#export psw1="$1"
 
 a=$(aws ec2 describe-instances --region ap-south-1 --filters "Name=tag:Env,Values=prod" --query 'Reservations[].Instances[].PublicIpAddress' --output text)
 
