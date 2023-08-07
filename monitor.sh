fbf7fca7885dab0e72676523f0b550dc2ee7e0a5
Muthu ramalingam <128946991+Muthuinc@users.noreply.github.com>
2023-08-07 11:37:44 +0530

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -7,13 +7,17 @@
 
 a=$(aws ec2 describe-instances --region ap-south-1 --filters "Name=tag:Env,Values=prod" --query 'Reservations[].Instances[].PublicIpAddress' --output text)
 
-scp -o StrictHostKeyChecking=no -i "$psw1" blackbox.yml ubuntu@$a:/home/ubuntu
+sed -i "s/EC2_ip/$a/g" prometheus.yml
 
-scp -o StrictHostKeyChecking=no -i "$psw1" prometheus.yml ubuntu@$a:/home/ubuntu
+scp -o StrictHostKeyChecking=no -i "$SSH_KEY" blackbox.yml "$ubuntu"@$a:/home/ubuntu/blackbox
 
-scp -o StrictHostKeyChecking=no -i "$psw1" docker-compose1.yml ubuntu@$a:/home/ubuntu
+scp -o StrictHostKeyChecking=no -i "$SSH_KEY" prometheus.yml "$ubuntu"@$a:/home/ubuntu/prometheus
 
-ssh -o StrictHostKeyChecking=no -i "$psw1" ubuntu@$a <<EOF
+scp -o StrictHostKeyChecking=no -i "$SSH_KEY" grafana.ini "$ubuntu"@$a:/home/ubuntu/grafana
+
+scp -o StrictHostKeyChecking=no -i "$SSH_KEY" docker-compose1.yml "$ubuntu"@$a:/home/ubuntu
+
+ssh -o StrictHostKeyChecking=no -i "$SSH_KEY" "$ubuntu"@$a <<EOF
 
 sudo docker-compose1 up -d
 
